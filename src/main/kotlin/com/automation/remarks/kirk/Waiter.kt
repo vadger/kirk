2eed21311814d296ab89d7e5faaa6b3607dc6f3a
SergeyPirogov <Semen4ik20>
2017-06-29 13:06:45 +0300

    commented wait deleted from waiter

--- src/main/kotlin/com/automation/remarks/kirk/Waiter.kt
+++ src/main/kotlin/com/automation/remarks/kirk/Waiter.kt
@@ -8,38 +8,6 @@ import org.openqa.selenium.TimeoutException
 /**
  * Created by sergey on 25.06.17.
  */
-//fun waitFor(elements: KElementCollection, condition: CollectionCondition, timeout: Int = 4000, poolingInterval: Long = 0.1.toLong()): Boolean {
-//    val endTime = System.currentTimeMillis() + timeout
-//    while (true) {
-//        try {
-//            return condition.evaluate(elements.webElements)
-//        } catch (ex: ConditionMismatchException) {
-//            if (System.currentTimeMillis() > endTime) {
-//                val message = """
-//            failed while waiting ${timeout / 1000} seconds
-//            to assert $condition
-//            for all elements located {$elements}
-//            reason: ${ex.message}
-//                        """
-//                throw TimeoutException(message)
-//            }
-//            Thread.sleep(poolingInterval)
-//        } catch (ex: org.openqa.selenium.NoSuchElementException) {
-//            if (System.currentTimeMillis() > endTime) {
-//                val message = """
-//            failed while waiting ${timeout / 1000} seconds
-//            for existence of all elements {$elements}
-//            reason:
-//                either wrong locator
-//                or did not have time to load
-//                """
-//                throw TimeoutException(message)
-//            }
-//            Thread.sleep(poolingInterval)
-//        }
-//    }
-//}
-
 fun <T> waitFor(locator: ElementLocator<T>, condition: Condition<T>, timeout: Int = 4000, poolingInterval: Long = 0.1.toLong()) {
     val endTime = System.currentTimeMillis() + timeout
     while (true) {
