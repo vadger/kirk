4e7273fbc9c9c03af418545ff6164391c0bd84c5
SergeyPirogov <Semen4ik20>
2017-06-30 16:57:10 +0300

    condition exception test added

--- src/test/kotlin/TestBrowser.kt
+++ src/test/kotlin/TestBrowser.kt
@@ -1,7 +1,6 @@
 import com.automation.remarks.kirk.Browser.Companion.drive
 import com.automation.remarks.kirk.conditions.be
 import com.automation.remarks.kirk.conditions.have
-import org.openqa.selenium.TimeoutException
 import org.testng.Assert.assertEquals
 import org.testng.annotations.Test
 
@@ -36,64 +35,6 @@ class TestBrowser : BaseTest() {
     }
 
     @Test
-    fun testShouldThrowTimeOutException() {
-        assertExceptionThrown(TimeoutException::class) {
-            drive {
-                to(url)
-                element("#header").should(have.text("KIr"))
-            }
-        }
-    }
-
-    @Test
-    fun testShouldThrowExceptionAtInvisibleElement() {
-        assertExceptionThrown(TimeoutException::class) {
-            drive {
-                to(url)
-                element("#input_invisible").setVal("demo")
-            }
-        }
-    }
-
-    @Test
-    fun testShouldFailOnWrongSelector() {
-        assertExceptionThrown(TimeoutException::class) {
-            drive {
-                to(url)
-                element("#wrongLocator").should(be.visible)
-            }
-        }
-    }
-
-    @Test
-    fun testShouldFindCollectionOfElements() {
-        drive {
-            to(url)
-            all("li").should(have.collectionSize(3))
-        }
-    }
-
-    @Test
-    fun testShouldThrowExceptionOnWrongNumberOfElements() {
-        assertExceptionThrown(TimeoutException::class) {
-            drive {
-                to(url)
-                all("li").should(have.collectionSize(2))
-            }
-        }
-    }
-
-    @Test
-    fun testShouldFailOnWrongCollectionLocator() {
-        assertExceptionThrown(TimeoutException::class) {
-            drive {
-                to(url)
-                all("@wrongLocator").should(have.collectionSize(5))
-            }
-        }
-    }
-
-    @Test
     fun shouldIterateOverCollection() {
         drive {
             to(url)
@@ -103,11 +44,4 @@ class TestBrowser : BaseTest() {
         }
     }
 
-    @Test
-    fun shouldCompareCollectionText() {
-        drive {
-            to(url)
-            all("li").should(have.exactText("Один", "Два", "Три"))
-        }
-    }
 }
