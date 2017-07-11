1c56d885963c9b34e5816f055437a1a3889b41fd
Sergey Pirogov <sergio_89@ukr.net>
2017-07-11 20:28:49 +0300

    KElement extensions added

--- src/test/kotlin/com/automation/remarks/kirk/test/vanila/KElementTest.kt
+++ src/test/kotlin/com/automation/remarks/kirk/test/vanila/KElementTest.kt
@@ -1,6 +1,8 @@
 package com.automation.remarks.kirk.test.vanila
 
 import com.automation.remarks.kirk.Browser
+import com.automation.remarks.kirk.KElement
+import com.automation.remarks.kirk.conditions.have
 import com.automation.remarks.kirk.core.drive
 import com.automation.remarks.kirk.test.BaseTest
 import org.testng.annotations.Test
@@ -13,8 +15,21 @@ class KElementTest : BaseTest(){
     @Test fun testCanCombine(){
         Browser.drive {
             to(url)
-            element(".paginator")
+            s("ul").firstChild().should(have.text("Один"))
+            s("ul").lastChild().should(have.text("Три"))
         }
     }
 
+}
+
+
+fun Browser.s(cssLocator:String): KElement {
+    return element(cssLocator)
+}
+fun KElement.firstChild(): KElement {
+    return this.element(":first-child")
+}
+
+fun KElement.lastChild(): KElement {
+    return this.element(":last-child")
 }
