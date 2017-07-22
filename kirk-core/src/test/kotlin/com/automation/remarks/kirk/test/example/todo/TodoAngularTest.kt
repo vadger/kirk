705c9794681a14b4fb81f89e202241a0d1614f24
Sergey Pirogov <sergio_89@ukr.net>
2017-07-22 12:06:31 +0300

    example update

--- kirk-core/src/test/kotlin/com/automation/remarks/kirk/test/example/todo/TodoAngularTest.kt
+++ kirk-core/src/test/kotlin/com/automation/remarks/kirk/test/example/todo/TodoAngularTest.kt
@@ -1,6 +1,6 @@
 package com.automation.remarks.kirk.test.example.todo
 
-import com.automation.remarks.kirk.Browser
+import com.automation.remarks.kirk.Browser.Companion.open
 import com.automation.remarks.kirk.conditions.have
 import org.testng.annotations.Test
 
@@ -10,25 +10,22 @@ import org.testng.annotations.Test
 // tag::TodoAngularTest[]
 class TodoAngularTest {
     @Test fun testCanAddNewTaskAndDelete() {
-        Browser.drive {
-            to(::TodoPage) {
-                addTasks("Item0")
-                taskList.should(have.size(1))
-                deleteTask("Item0")
-                taskList.should(have.size(0))
-            }
+        open(::TodoPage) {
+            addTasks("Item0")
+            taskList.should(have.size(1))
+            deleteTask("Item0")
+            taskList.should(have.size(0))
         }
     }
 
+
     @Test fun testCanDeactivateTask() {
-        Browser.drive {
-            to(::TodoPage) {
-                addTasks("A", "B", "C")
-                deactivateTask("A")
-                counter.should(have.text("2"))
-                goToCompletedTab()
-                taskList.should(have.exactText("A"))
-            }
+        open(::TodoPage) {
+            addTasks("A", "B", "C")
+            deactivateTask("A")
+            counter.should(have.text("2"))
+            goToCompletedTab()
+            taskList.should(have.exactText("A"))
         }
     }
 }
