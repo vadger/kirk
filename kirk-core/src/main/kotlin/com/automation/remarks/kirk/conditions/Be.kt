0d64fc78c95f053e36a4e86cd87d5608457d187c
Alexey Panashchenko <alpa@ciklum.com>
2017-07-27 18:01:18 +0300

    visible is bad #44

--- kirk-core/src/main/kotlin/com/automation/remarks/kirk/conditions/Be.kt
+++ kirk-core/src/main/kotlin/com/automation/remarks/kirk/conditions/Be.kt
@@ -1,16 +1,15 @@
 package com.automation.remarks.kirk.conditions
 
-import org.openqa.selenium.WebElement
 
 /**
  * Created by sergey on 25.06.17.
  */
 class Be {
 
-    val visible = Visibility()
+    @JvmField val visible = Visibility()
 
-    val invisible = Not(Visibility())
-    val empty = CollectionSize(0)
+    @JvmField val invisible = Not(Visibility())
+    @JvmField val empty = CollectionSize(0)
 }
 
 @JvmField
