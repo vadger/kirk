6fa6e5ad438402e135f9d8ce80b7b3e698aa6897
SergeyPirogov <Semen4ik20>
2017-08-11 15:12:34 +0300

    wd factory tests added

---
+++ kirk-core/src/test/kotlin/com/automation/remarks/kirk/test/WebdriverFactoryTest.kt
@@ -0,0 +1,54 @@
+package com.automation.remarks.kirk.test
+
+import com.automation.remarks.kirk.Configuration
+import com.automation.remarks.kirk.core.WebDriverFactory
+import com.automation.remarks.kirk.core.configuration
+import com.automation.remarks.kirk.core.loadConfig
+import org.aeonbits.owner.Config
+import org.openqa.grid.selenium.GridLauncherV3
+import org.testng.annotations.BeforeClass
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
+
+/**
+ * Created by sepi on 8/11/2017.
+ */
+class WebDriverFactoryTest : BaseTest() {
+
+    @BeforeClass
+    fun setUp() {
+        val hub = arrayOf("-port", "4444", "-host", "localhost", "-role", "hub")
+        GridLauncherV3.main(hub)
+
+        val node = arrayOf("-port", "5555", "-host", "localhost", "-role", "node", "-hub", "http://localhost:4444/grid/register")
+        GridLauncherV3.main(node)
+    }
+
+    @Test
+    fun testCanSpinFirefox() {
+        configuration = loadConfig(Firefox::class)
+        val driverFactory = WebDriverFactory()
+        me.tatarka.assertk.assert(driverFactory.getDriver() is Firefox)
+    }
+
+    @Test
+    fun testCanSpinRemoteDriver() {
+        configuration = loadConfig(RemoteDriver::class)
+        val driverFactory = WebDriverFactory()
+        me.tatarka.assertk.assert(driverFactory.getDriver() is Firefox)
+    }
+}
+
+interface Firefox : Configuration {
+    @Config.DefaultValue("firefox")
+    override fun browserName(): String
+}
+
+interface RemoteDriver : Configuration {
+    @Config.Key("kirk.remote.url")
+    @Config.DefaultValue("http://localhost:4444/wd/hub")
+    override fun remoteUrl(): String
+
+    @Config.DefaultValue("firefox")
+    override fun browserName(): String
+}
